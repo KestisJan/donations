94455803e566a8d2c17fbcba3e94e086fd66e21d
Kestas <kestutisjankevicius9@gmail.com>
2023-11-17 15:12:48 +0200

    Role & Permissions create/store functions

---
+++ resources/views/admin/permissions/create.blade.php
@@ -0,0 +1,29 @@
+<x-admin-layout>
+    <!-- Permissions Create -->
+
+    <div class="py-12 w-full">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="flex p-2">
+                    <a href="{{ route('admin.permissions.index') }}" class="px-4 py-2 bg-green-700 hover:bg-green-500 text-slate-100 rounded-md">Role Index</a>
+                </div>
+                <div class="flex flex-col">
+                    <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
+                        <form method="POST" action="{{ route('admin.permissions.store')}}">
+                         @csrf
+                            <div class="sm:col-span-6">
+                                <label for="name" class="block text-sm font-medium text-gray-700"></label>
+                                <div class="mt-1">
+                                    <input type="text" class="block w-full" name="name" class="block w-full">
+                                </div>
+                            </div>
+                            <div class="sm:col-span-6 pt-5">
+                                <button type="submit" class="ox-4 py-2 bg-green-900 hover:bg-green-700 rounded-md">Create</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -5,7 +5,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="flex justify-end p-2">
-                    <a href="#" class="px-4 py-2 bg-green-700 hover:bg-green-500 rounded-md">Create Role</a>
+                    <a href="{{ route('admin.roles.create') }}" class="px-4 py-2 bg-green-700 hover:bg-green-500 rounded-md">Create Role</a>
                 </div>
                 <div class="flex flex-col">
                     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -17,7 +17,7 @@
                                         <th scope="col">Role</th>
                                       </tr>
                                     </thead>
-                                    <tbody>
+                                    <tbody class="justify-content">
                                      @foreach ($roles as $role)
                                      <tr>
                                         <td class="px-6 py-4 whitespace-nowrap">
