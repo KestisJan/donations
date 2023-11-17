035aa050685c62fe2b7503c20ad416a244ab8c3f
Kestas <kestutisjankevicius9@gmail.com>
2023-11-17 14:43:42 +0200

    Admin dashboard & Roles

---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,43 @@
+<x-admin-layout>
+    <!-- Roles INDEX -->
+
+    <div class="py-12 w-full">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="flex justify-end p-2">
+                    <a href="#" class="px-4 py-2 bg-green-700 hover:bg-green-500 rounded-md">Create Role</a>
+                </div>
+                <div class="flex flex-col">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table class="table">
+                                    <thead class="thead-light">
+                                      <tr>
+                                        <th scope="col">Role</th>
+                                      </tr>
+                                    </thead>
+                                    <tbody>
+                                     @foreach ($roles as $role)
+                                     <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                {{ $role->name }}
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <a href="">Edit</a>
+                                            <a href="">Delete</a>
+                                        </td>
+                                     </tr>
+                                     @endforeach
+                                    </tbody>
+                                  </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
